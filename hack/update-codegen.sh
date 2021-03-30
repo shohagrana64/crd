170cabc5e03d386e37597bbd85129e993cab6411
shohag <shohagrana@appscode.com>
2021-03-30 13:33:30 +0600

    initial commit

---
+++ hack/update-codegen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+vendor/k8s.io/code-generator/generate-groups.sh all \
+github.com/shohagrana64/crd/pkg/client \
+github.com/shohagrana64/crd/pkg/apis \
+stable.example.com:v1alpha1
+--go-header-file "./hack/boilerplate.go.txt"
