be685464edad7ba83e7cec24ec4bf4eee582369c
shohag <shohagrana@appscode.com>
2021-03-30 15:41:30 +0600

    edit types.go

--- pkg/apis/stable.example.com/v1alpha1/types.go
+++ pkg/apis/stable.example.com/v1alpha1/types.go
@@ -1,3 +1,5 @@
+// +groupName=stable.example.com
+// +kubebuilder:object:generator=true
 package v1alpha1
 
 import (
@@ -5,35 +7,57 @@ import (
 )
 
 // +kubebuilder:object:root=true
-// +kubebuilder:resource:path=kubeapis,singular:kubeapi,shortName=kapi,categories={api,all}
-// +kubebuilder:subresources:status
-// +kubebuilder:printcolumen:name="Version",type="string",JSONPath=".spec.version"
-// +kubebuilder:printcolumn:name="Status",type="string",JSONPATH=".spec.phase"
-// +kubebuilder:printcolumn:name="Age",type="date",JSONPATH=".metadata.creationTimestamp"
-
+// +kubebuilder:resource:path=kubeapis,singular=kubeapi,shortName=kapi,categories={}
+// +kubebuilder:printcolumn:JSONPath=".metadata.creationTimestamp",name=Age,type=date
+// +kubebuilder:printcolumn:JSONPath=".status.deploymentName",name=Deployment,type=string
+// +kubebuilder:printcolumn:JSONPath=".status.serviceName",name=Service,type=string
+// +kubebuilder:printcolumn:JSONPath=".status.replicas",name=Replicas,type=integer
+// +kubebuilder:printcolumn:JSONPath=".status.phase",name=Status,type=string
+// +kubebuilder:subresource:status
+// +kubebuilder:storageversion
 // +genclient
-// +genclient:noStatus
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 type Disappointment struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   DisappointmentSpec   `json:"spec"`
+	Spec DisappointmentSpec `json:"spec"`
+	// +optional
 	Status DisappointmentStatus `json:"status"`
 }
 
 type DisappointmentSpec struct {
-	Version     string        `json:"version"`
-	Replicas    *int32        `json:"replicas,omitempty"`
-	HostUrl     string        `json:"hostUrl"`
-	ServiceType string        `json:"serviceType"`
-	Container   ContainerSpec `json:"container"`
+	// +optional
+	Version string `json:"version,omitempty"`
+
+	// +kubebuilder:default=1
+	// +optional
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=20
+	Replicas *int32 `json:"replicas"`
+
+	// +optional
+	DeploymentName string `json:"deploymentName"`
+
+	// +optional
+	ServiceName string `json:"serviceName"`
+
+	// +kubebuilder:default=ClusterIP
+	ServiceType ServiceType `json:"serviceType"`
+
+	Container ContainerSpec `json:"container"`
 }
 
+// +kubebuilder:validation:Enum=ClusterIP;NodePort
+type ServiceType string
+
 type ContainerSpec struct {
-	Image         string `json:"image"`
-	ContainerPort int32  `json:"containerPort"`
+	// Container image of the Api Server
+	Image string `json:"image"`
+
+	// Container port of Api Server
+	ContainerPort int32 `json:"containerPort"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
@@ -45,6 +69,6 @@ type DisappointmentList struct {
 }
 
 type DisappointmentStatus struct {
-	// +optional
-	Phase string `json:"phase,omitempty"`
+	Phase    string `json:"phase"`
+	Replicas int32  `json:"replicas"`
 }
